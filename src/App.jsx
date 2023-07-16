5b28ad4467b8d97e814773b803a395f84ef8e0c4
abdallah <stugfybb@gmail.com>
2023-07-16 20:44:17 +0300

    add the best route practise

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainPorfolio/>}/>
+        <Route element={<MainPorfolio/>}/>
         <Route path="*" element={<PageNotFound/>}/>
       </Routes>
     </Router>
