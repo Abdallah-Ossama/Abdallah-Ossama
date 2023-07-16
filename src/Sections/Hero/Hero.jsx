49c296867ce42e3aa6ffdf44223559d37e7e70d4
abdallah <stugfybb@gmail.com>
2023-07-16 20:40:51 +0300

    //yes

--- src/Sections/Hero/Hero.jsx
+++ src/Sections/Hero/Hero.jsx
@@ -1,4 +1,3 @@
-// import { motion } from "framer-motion";
 import "./Hero.css";
 import Main_bg from "../../assets/images/test/full home ,mountains.webp"
 import Stars from "../../assets/images/stars1.webp";
@@ -12,18 +11,20 @@ import metoret2 from "../../assets/images/metoret2.svg";
 const Hero = () => {
   return (
     <section id="Hero">
-        <Container/>
-          <img className="metoret1" src={metoret1} alt="metoret"/>
-          <img className="metoret2" src={metoret2} alt="metoret"/>
-        <img src={Main_bg} id="main-bg" alt="bg"/>
-        <img src={Stars} id="main-stars" alt="stars"/>
-        <img src={BottomLand} id="bottom-land" alt="bottomLand"/>
-        <div id="moon">
-          <div id="div1"/>
-          <div id="div2"/>
-          <div id="div3"/>
-        </div>
-        <Chat/>
+      <Container/>
+
+        <img className="metoret1" src={metoret1} alt="metoret"/>
+        <img className="metoret2" src={metoret2} alt="metoret"/>
+      <img src={Main_bg} id="main-bg" alt="bg"/>
+      <img src={Stars} id="main-stars" alt="stars"/>
+      <img src={BottomLand} id="bottom-land" alt="bottomLand"/>
+      <div id="moon">
+        <div id="div1"/>
+        <div id="div2"/>
+        <div id="div3"/>
+      </div>
+      
+      <Chat/>
     </section>
   )
 }
